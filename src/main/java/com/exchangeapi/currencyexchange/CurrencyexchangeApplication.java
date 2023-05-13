d1051707d48ac1b996994a460502cc45e5ebee1a
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2023-05-13 19:04:02 +0300

    Task 1 : Initial Commit

---
+++ src/main/java/com/exchangeapi/currencyexchange/CurrencyexchangeApplication.java
@@ -0,0 +1,13 @@
+package com.exchangeapi.currencyexchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyexchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyexchangeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exchangeapi/currencyexchange/CurrencyexchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exchangeapi.currencyexchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyexchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
