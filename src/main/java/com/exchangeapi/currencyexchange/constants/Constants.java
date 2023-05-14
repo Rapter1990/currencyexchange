5dc8e27a13c283c69ec49daeaf2ddf7e171b4cb6
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2023-05-14 03:01:19 +0300

    Task 18 : Revise Constants

--- src/main/java/com/exchangeapi/currencyexchange/constants/Constants.java
+++ src/main/java/com/exchangeapi/currencyexchange/constants/Constants.java
@@ -12,22 +12,22 @@ public class Constants {
     public static String EXCHANGE_CACHE_NAME;
     public static Integer EXCHANGE_API_CALL_LIMIT;
 
-    @Value("${exchange.api-url}")
+    @Value("${exchange-api.api-url}")
     public void setExchangeApiBaseUrl(String apiUrl) {
         Constants.EXCHANGE_API_BASE_URL = apiUrl;
     }
 
-    @Value("${exchange.api-key}")
+    @Value("${exchange-api.api-key}")
     public void setExchangeApiKey(String apiKey) {
         EXCHANGE_API_API_KEY = apiKey;
     }
 
-    @Value("${exchange.cache-name}")
+    @Value("${exchange-api.cache-name}")
     public void setExchangeCacheName(String cacheName) {
         Constants.EXCHANGE_CACHE_NAME = cacheName;
     }
 
-    @Value("${exchange.api-call-limit}")
+    @Value("${exchange-api.api-call-limit}")
     public void setExchangeApiCallLimit(Integer apiCallLimit) {
         EXCHANGE_API_CALL_LIMIT = apiCallLimit;
     }
