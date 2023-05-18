c3b3d28be1630267abdefca50934c437c7f37fe7
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2023-05-18 19:53:40 +0300

    Task 22 : Implement ExchangeService and RateService

---
+++ src/main/java/com/exchangeapi/currencyexchange/service/ExchangeService.java
@@ -0,0 +1,102 @@
+package com.exchangeapi.currencyexchange.service;
+
+import com.exchangeapi.currencyexchange.dto.ExchangeDto;
+import com.exchangeapi.currencyexchange.dto.RateDto;
+import com.exchangeapi.currencyexchange.dto.RateInfoDto;
+import com.exchangeapi.currencyexchange.entity.ExchangeEntity;
+import com.exchangeapi.currencyexchange.entity.enums.EnumCurrency;
+import com.exchangeapi.currencyexchange.exception.ExchangeNotFoundException;
+import com.exchangeapi.currencyexchange.repository.ExchangeRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.annotation.PostConstruct;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+
+@Service
+@CacheConfig(cacheNames = {"exchanges"})
+@AllArgsConstructor
+@Slf4j
+public class ExchangeService {
+
+    private final ExchangeRepository exchangeRepository;
+    private final RateService rateService;
+    private final ObjectMapper objectMapper;
+
+
+    @CachePut(key = "#exchange")
+    public ExchangeDto calculateExchangeRate(Double amount, EnumCurrency base, List<EnumCurrency> targets) {
+        log.info("ExchangeService | calculateExchangeRate is called");
+
+        RateDto rateDto = rateService.calculateRate(base,targets);
+
+        Map<EnumCurrency, Double> exchanges = rateDto.getRates()
+                .stream()
+                .map(rateInfoDto -> mapToRateInfoDto(rateInfoDto,amount))
+                .collect(Collectors.toMap(RateInfoDto::currency,RateInfoDto::rate));
+
+        ExchangeEntity exchangeEntity = ExchangeEntity.builder()
+                .base(rateDto.getBase())
+                .rates(exchanges)
+                .amount(amount)
+                .date(LocalDate.now())
+                .build();
+
+        ExchangeEntity savedExchange = exchangeRepository.saveAndFlush(exchangeEntity);
+
+        return mapToExchangeDTO(savedExchange);
+    }
+
+
+    @Cacheable(key = "#exchange")
+    public ExchangeDto getConversion(Long id) {
+        log.info("ExchangeService | calculateExchangeRate is called");
+        return exchangeRepository.findById(id).map(this::mapToExchangeDTO).orElseThrow(() -> new ExchangeNotFoundException("Not Found"));
+    }
+
+    public List<ExchangeDto> getConversionList(LocalDate startDate, LocalDate endDate) {
+        log.info("ExchangeService | getConversionList is called");
+        return exchangeRepository.findByDateBetween(startDate, endDate).stream()
+                .map(this::mapToExchangeDTO)
+                .collect(Collectors.toList());
+    }
+
+    private ExchangeDto mapToExchangeDTO(ExchangeEntity exchangeEntity) {
+
+        return ExchangeDto.builder()
+                .amount(exchangeEntity.getAmount())
+                .base(exchangeEntity.getBase())
+                .date(exchangeEntity.getDate())
+                .build();
+
+    }
+
+    private RateInfoDto mapToRateInfoDto(RateInfoDto rateInfoDto, Double amount) {
+
+        EnumCurrency enumCurrency = rateInfoDto.currency();
+        Double rate = BigDecimal.valueOf(rateInfoDto.rate()).multiply(BigDecimal.valueOf(amount),
+                new MathContext(5, RoundingMode.CEILING)).doubleValue();
+        return new RateInfoDto(enumCurrency,rate);
+    }
+
+    @CacheEvict(allEntries = true)
+    @PostConstruct
+    @Scheduled(fixedRateString = "${exchange-api.cache-ttl}")
+    public void clearCache(){
+        log.info("Caches are cleared");
+    }
+}
---
+++ src/main/java/com/exchangeapi/currencyexchange/service/RateService.java
@@ -0,0 +1,118 @@
+package com.exchangeapi.currencyexchange.service;
+
+import com.exchangeapi.currencyexchange.dto.RateDto;
+import com.exchangeapi.currencyexchange.dto.RateInfoDto;
+import com.exchangeapi.currencyexchange.entity.RateEntity;
+import com.exchangeapi.currencyexchange.entity.enums.EnumCurrency;
+import com.exchangeapi.currencyexchange.payload.response.RateResponse;
+import com.exchangeapi.currencyexchange.repository.RateRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
+
+import static com.exchangeapi.currencyexchange.constants.Constants.EXCHANGE_API_API_KEY;
+import static com.exchangeapi.currencyexchange.constants.Constants.EXCHANGE_API_BASE_URL;
+
+@Service
+@AllArgsConstructor
+@Slf4j
+public class RateService {
+
+    private final RateRepository rateRepository;
+    private final RestTemplate restTemplate;
+
+    private final ObjectMapper objectMapper;
+
+    public RateDto calculateRate() {
+        log.info("ExchangeService | calculateRates() is called");
+        return calculateRate(null);
+    }
+
+    public RateDto calculateRate(EnumCurrency base) {
+        log.info("ExchangeService | calculateRates(EnumCurrency base) is called");
+        return calculateRate(base, null);
+    }
+
+    public RateDto calculateRate(EnumCurrency base, List<EnumCurrency> targets) {
+        log.info("ExchangeService | calculateRates(EnumCurrency base, List<EnumCurrency> targets) is called");
+        return calculateRate(base, targets, null);
+    }
+
+    public RateDto calculateRate(EnumCurrency base, List<EnumCurrency> targets, LocalDate date) {
+        log.info("ExchangeService | calculateRates is called");
+
+        EnumCurrency baseCurrency = Optional.ofNullable(base).orElse(EnumCurrency.EUR);
+        List<EnumCurrency> targetsCurrency = Optional.ofNullable(targets)
+                .orElseGet(() -> Arrays.asList(EnumCurrency.values()));
+
+        LocalDate rateDate = Optional.ofNullable(date).orElse(LocalDate.now());
+
+        RateEntity rateEntity = rateRepository.findOneByDate(rateDate)
+                .orElseGet(() -> saveRatesFromApi(rateDate));
+
+        Map<EnumCurrency, Double> rates = rateEntity.getRates();
+
+        BigDecimal baseCurrencyRate = BigDecimal.valueOf(rates.get(baseCurrency));
+
+        List<RateInfoDto> rateList = rates.entrySet()
+                .stream()
+                .filter(entry -> targetsCurrency.contains(entry.getKey()))
+                .map(entryToRate(baseCurrencyRate))
+                .toList();
+
+        return RateDto.builder()
+                .base(base)
+                .rates(rateList)
+                .date(rateDate)
+                .build();
+    }
+
+    private Function<Map.Entry<EnumCurrency, Double>, RateInfoDto> entryToRate(BigDecimal baseCurrencyRate) {
+        return entry -> new RateInfoDto(
+                entry.getKey(),
+                BigDecimal.valueOf(entry.getValue()).divide(baseCurrencyRate, 5, RoundingMode.CEILING).doubleValue()
+        );
+    }
+
+    private RateEntity saveRatesFromApi(LocalDate rateDate) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("apikey", EXCHANGE_API_API_KEY);
+        ResponseEntity<String> response = restTemplate.exchange(getExchangeUrl(), HttpMethod.GET, null, String.class, headers);
+        try {
+            RateResponse rates = objectMapper.readValue(response.getBody(), RateResponse.class);
+            RateEntity entity = convert(rates);
+            entity.setDate(rateDate);
+            return rateRepository.save(entity);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String getExchangeUrl() {
+        return EXCHANGE_API_BASE_URL;
+    }
+
+    private RateEntity convert(RateResponse source) {
+
+        return RateEntity.builder()
+                .base(source.base())
+                .date(source.date())
+                .build();
+
+    }
+}
