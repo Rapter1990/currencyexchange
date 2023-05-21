60215dcdfca30e00b74c853765205fd7ebfd4783
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2023-05-21 17:23:39 +0300

    Task 27 : Add ConversionController and Currency Exchange API.postman_collection.json

---
+++ src/main/java/com/exchangeapi/currencyexchange/controller/ConversionController.java
@@ -0,0 +1,34 @@
+package com.exchangeapi.currencyexchange.controller;
+
+import com.exchangeapi.currencyexchange.dto.ExchangeDto;
+import com.exchangeapi.currencyexchange.service.ExchangeService;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/conversion")
+@AllArgsConstructor
+public class ConversionController {
+
+    private final ExchangeService exchangeService;
+
+    @RateLimiter(name = "basic")
+    @GetMapping("/{id}")
+    public ResponseEntity<ExchangeDto> getConversion(@PathVariable(name = "id") Long id) {
+        return ResponseEntity.ok(exchangeService.getConversion(id));
+    }
+
+    @RateLimiter(name = "basic")
+    @GetMapping
+    public ResponseEntity<List<ExchangeDto>> getConversionList(@RequestParam(name = "startDate")  LocalDate startDate,
+                                                               @RequestParam(name = "endDate") LocalDate endDate) {
+
+        return ResponseEntity.ok(exchangeService.getConversionList(startDate,
+                endDate));
+    }
+}
