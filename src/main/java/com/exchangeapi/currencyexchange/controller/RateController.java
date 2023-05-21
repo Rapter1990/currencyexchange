2c2db85248b1464403ce7bc6cca7ce477c1bd8a3
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2023-05-21 16:38:22 +0300

    Task 25 : Revise RateController

--- src/main/java/com/exchangeapi/currencyexchange/controller/RateController.java
+++ src/main/java/com/exchangeapi/currencyexchange/controller/RateController.java
@@ -5,6 +5,7 @@ import com.exchangeapi.currencyexchange.entity.enums.EnumCurrency;
 import com.exchangeapi.currencyexchange.service.RateService;
 import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +17,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/v1/rate")
 @AllArgsConstructor
+@Slf4j
 public class RateController {
 
     private final RateService rateService;
@@ -25,7 +27,7 @@ public class RateController {
     public ResponseEntity<RateDto> getRates(@RequestParam(name = "base",required = false) EnumCurrency base,
                             @RequestParam(name = "target",required = false) List<EnumCurrency> target,
                             @RequestParam(name = "date",required = false) @DateTimeFormat(pattern = "yyyy-mm-dd") LocalDate date) {
-
+        log.info("RateController | getRates is called");
         return ResponseEntity.ok(rateService.calculateRate(base, target,date));
     }
 }
