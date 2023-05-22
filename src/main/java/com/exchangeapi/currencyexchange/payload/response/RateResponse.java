7e9931c7159a4cb44cbec1a7691fb47f1769f691
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2023-05-22 15:10:04 +0300

    Task 37 : Revise RateResponse

--- src/main/java/com/exchangeapi/currencyexchange/payload/response/RateResponse.java
+++ src/main/java/com/exchangeapi/currencyexchange/payload/response/RateResponse.java
@@ -1,10 +1,12 @@
 package com.exchangeapi.currencyexchange.payload.response;
 
 import com.exchangeapi.currencyexchange.entity.enums.EnumCurrency;
+import lombok.Builder;
 
 import java.time.LocalDate;
 import java.util.Map;
 
+@Builder
 public record RateResponse(EnumCurrency base,
                            LocalDate date,
                            Map<EnumCurrency, Double> rates,
