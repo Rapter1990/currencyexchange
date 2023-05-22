cbed106549b169d9e737b9b53aaa7ec6dc04007d
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2023-05-22 15:09:44 +0300

    Task 36 : Add BaseControllerTest and BaseServiceTest

---
+++ src/test/java/com/exchangeapi/currencyexchange/base/BaseControllerTest.java
@@ -0,0 +1,7 @@
+package com.exchangeapi.currencyexchange.base;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest
+public abstract class BaseControllerTest {
+}
